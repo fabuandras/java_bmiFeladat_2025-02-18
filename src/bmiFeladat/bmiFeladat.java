c0d9074faf8f612f30f147ddf387966cec7446be
fabuandras <fabuandras@gmail.com>
2025-02-18 15:42:24 +0100

    bmiFeladat - kesz

---
+++ src/bmiFeladat/bmiFeladat.java
@@ -0,0 +1,53 @@
+package bmiFeladat;
+
+import java.util.Scanner;
+
+public class bmiFeladat {
+
+    public static void main(String[] args) {
+        bmiFeladat();
+    }
+    
+    public static void bmiFeladat() {
+        kiir("BMI kiszámítása");
+        System.out.println("-------------------------");
+        int m = beker("Testmagasság (cm)");
+        int t = beker("Testtömeg (kg)");
+        double bmi = bmiSzamit(t, m);
+        System.out.println("-------------------------");
+        ertekeles(bmi);
+    }
+    
+    public static void kiir(String szoveg){
+        System.out.println(szoveg);
+    }
+    
+    public static double bmiSzamit(int t, int m){
+        return t / Math.pow(m / 100.0, 2);
+    }
+    
+    public static int beker(String kerdes){
+        Scanner scn = new Scanner(System.in);
+        kiir(kerdes + ": ");
+        return scn.nextInt();
+    }
+    
+    public static void ertekeles(double bmi){
+        kiir("Az Ön BMI értéke: " + bmi);
+        if (bmi < 20) {
+            System.out.println("- Sovány");
+        }
+        else if (bmi < 25) {
+            System.out.println("- Normál");
+        }
+        else if (bmi < 30) {
+            System.out.println("- Túlsúlyos");
+        }
+        else if (bmi < 40) {
+            System.out.println("- Elhízott");
+        }
+        else {
+            System.out.println("- Kórosan elhízott");
+        }
+    }
+}
